ad814505ae46774dca644795ec4f08efb548c8dd
sanjeev <sanjeevmaths1729@gmail.com>
2023-12-20 22:32:11 +0530

    merging to master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Player player1=new Player();
         Board gameBoard=new Board();
+
+        System.out.println("Player1 Rolling the dice");
+        System.out.println(gameBoard.RollDice());
     }
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,8 +1,9 @@
 public class Player {
     int position;
-
+    int moves;
     public Player(){
         position=0;
+        moves=0;
     }
 
     public boolean isWinner(){
