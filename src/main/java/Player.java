beef849ae7b0af9b7f2ce82818f36887c221991b
sanjeev <sanjeevmaths1729@gmail.com>
2023-12-20 22:48:02 +0530

    use Case 3 and use case 4 both done

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Player {
     int position;
     int moves;
@@ -8,6 +10,30 @@ public class Player {
 
     public boolean isWinner(){
         return position==100;
+    }
+
+    public int RollDice(){
+        Random random=new Random();
+        return random.nextInt(6)+1;
+    }
+
+
+    public void makeMove(){
+        double draw=Math.random();
+        int diceVal=RollDice();
+        if(draw==.4) return;
+        else if(draw<.4){
+            this.position=max(0,this.position-diceVal);
+        }else{
+            if(this.position+diceVal<=100) this.position+=diceVal;
+        }
+    }
+
+    public void singlePlayerPlay(){
+        while(!isWinner()){
+            this.moves++;
+            makeMove();
+        }
     }
     
 }
